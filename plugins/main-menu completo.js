15c613261ebdc83c4407bcecd8b3af546177cc92
Matias <108229950+Matias-Bots@users.noreply.github.com>
2022-06-30 12:03:00 -0400

    Update main-menu completo.js

--- plugins/main-menu completo.js
+++ plugins/main-menu completo.js
@@ -3,7 +3,7 @@
 
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let duit = `*╭───── 「 𝐒𝐔𝐏𝐄𝐑𝐁𝐎𝐓 - 𝐌𝐃 」 ──────*
+let duit = `*╭───── 「 𝐏𝐀𝐑𝐀𝐆𝐔𝐀 - 𝐁𝐎𝐓  」 ──────*
 │ *menu completo*
 └────
 ╭─「 %category 」
@@ -19,7 +19,7 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
            hydratedButtons: [{
              urlButton: {
                displayText: 'grupo',
-               url: 'https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k'
+               url: 'https://chat.whatsapp.com/JIRYr6hK0gw2PusdUlsG0R'
              }
 
            },
@@ -32,7 +32,7 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
                {
              quickReplyButton: {
                displayText: '🧒 Owner',
-               id: '.owner',
+               url: 'https://wa.me/595985958156',
              }
 
            }]
