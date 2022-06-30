aa3abf328e5b4fdf9efc14707cad554fe37be5cf
Matias <108229950+Matias-Bots@users.noreply.github.com>
2022-06-30 12:09:48 -0400

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -8,8 +8,8 @@ let handler = async (m) => {
 let duit = `*────── 「 GRUPO」 ──────*
 
 *Hola 👋*
-*estos sos los grupo oficial del bot*
-*https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k*
+*este es el grupo oficial del bot*
+*https://chat.whatsapp.com/JIRYr6hK0gw2PusdUlsG0R*
 h`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/e1e0f0bc5a5229c348eda.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
@@ -34,7 +34,7 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
                {
              quickReplyButton: {
                displayText: 'Owner',
-               id: '.owner',
+               url: 'https://wa.me/595985958156',
              }
 
            }]
